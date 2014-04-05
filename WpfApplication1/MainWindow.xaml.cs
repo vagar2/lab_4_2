55b3f4c412dfb2156a7b4f5792f62ba5de00b294
vagar2 <vagar2@mail.ru>
2014-04-06 01:21:57 +0400

    Сделайте так, что если вы работаете с треугольником «по умолчанию», то поля для задания координат должны быть неактивны (серым цветом и не позволять ввод данных).

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -54,6 +54,45 @@ namespace WpfApplication1
                 textBox2.Text = Convert.ToString(T.get_B());
                 textBox3.Text = Convert.ToString(T.get_C());
             }
+            if ((radioButton2.IsChecked == false) && (radioButton1.IsChecked == false))
+            {
+                MessageBox.Show("Выберите способ задания треугольника");
+            }
+        }
+
+        private void radioButton2_Checked(object sender, RoutedEventArgs e)
+        {
+            textBox1.IsEnabled = false;
+            textBox2.IsEnabled = false;
+            textBox3.IsEnabled = false;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            tx1.IsEnabled = true;
+            tx2.IsEnabled = true;
+            tx3.IsEnabled = true;
+            ty1.IsEnabled = true;
+            ty2.IsEnabled = true;
+            ty3.IsEnabled = true;
+        }
+
+        private void radioButton1_Checked(object sender, RoutedEventArgs e)
+        {
+            textBox1.IsEnabled = true;
+            textBox2.IsEnabled = true;
+            textBox3.IsEnabled = true;
+            tx1.IsEnabled = false;
+            tx2.IsEnabled = false;
+            tx3.IsEnabled = false;
+            ty1.IsEnabled = false;
+            ty2.IsEnabled = false;
+            ty3.IsEnabled = false;
+            tx1.Text = "";
+            tx2.Text = "";
+            tx3.Text = "";
+            ty1.Text = "";
+            ty2.Text = "";
+            ty3.Text = "";
         }
     }
 }
